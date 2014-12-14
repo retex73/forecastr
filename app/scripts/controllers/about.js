592c690219f95830dd6c7acd23402d8005e10b95
Peter Williams <peterwilliams@MacBook-Pro.home>
2014-12-14 23:22:34 +0000

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name forecastrApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the forecastrApp
+ */
+angular.module('forecastrApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name forecastrApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the forecastrApp
+ */
+angular.module('forecastrApp')
+  .controller('MainCtrl', function ($scope) {
+    // $scope.awesomeThings = [
+    //   'HTML5 Boilerplate',
+    //   'AngularJS',
+    //   'Karma'
+    // ];
+    
+
+    $scope.name = "peter";  
+    
+  });
