5630b3ad9dd2c17f340eb4abca31296fc40c1723
unknown <peter.williams@L14417-LSG.4ucore.4ultd.co.uk>
2014-12-16 09:34:40 +0000

    implemented a cleaner version for angular of browser geolocation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,65 +9,25 @@ angular.module('forecastrApp')
         // $scope.name = "peter";  
         $scope.name = "test";
 
-        var Main = window.Main || {};
+        // var Main = window.Main || {};
 
-        Main = {
-            forecastUrl: "https://api.forecast.io/forecast/13232015d73f2d847200a4c70b20868b/",
-            lat: '',
-            lon: '',
-            locationSet: false,
-            setLocation: function() {
-                navigator.geolocation.getCurrentPosition(this.setCoords, this.locationError);
-            },
-            setCoords: function(position) {
-                console.log(position);
-
-                Main.lat = position.coords.latitude;
-                Main.lon = position.coords.longitude;
-                this.locationSet = true;
-            },
-            locationError: function(err) {
-                if (err.code == 1) {
-                    console.log('not permitted to receive location');
-                    // manually set the location to a default
-                    this.locationSet = true;
-                }
-            },
-
-            checkCoordsSet: function() {
-                if (!this.locationSet) {
-                    console.log('not yet');
-                    window.setTimeout(this.checkCoordsSet, 100);
-                } else {
-                    console.log('ready');
-                    // this.getWeatherUrl(); 
-                    Main.getWeatherUrl();
-
-                }
-            },
-
-
-
-            getWeatherUrl: function() {
-                console.log('here');
-                console.log('lat: ' + this.lat);
-                // var url = this.forecastUrl + this.lat + "," + this.lon + "," + Date.now(); 
-                // console.log("Url: " + url); 
-            }
-
-        };
-
-        // Main.setLocation();     
-
-        // Main.checkCoordsSet(); 
+        
+        var forecastUrl: "https://api.forecast.io/forecast/13232015d73f2d847200a4c70b20868b/"; 
+            
 
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position) {
                 $scope.$apply(function() {
-                    $scope.position = position;
-                    console.log($scope.position); 
+                    $scope.position = position; 
+                    Main.lat = position.coords.latitude; 
+                    console.log(Main.lat); 
                 });
             });
         }
 
+        
+
+
     });
+
+
